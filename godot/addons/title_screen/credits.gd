281a6f09df1220c2f419217735b495a12448f3c1
Logic Monkey <logic.monkey@protonmail.com>
2023-05-21 06:36:35 -0500

    UI: License information wrangled. More or less.

--- godot/addons/title_screen/credits.gd
+++ godot/addons/title_screen/credits.gd
@@ -2,16 +2,16 @@ extends ColorRect
 
 func _ready():
 	%license.text = Engine.get_license_text()
-	
 
+		
 
 func _on_title_pressed():
 	_FADE.FadeTo("res://addons/title_screen/title_screen.tscn")
 
 
-const SCROLL_AMT = 20
+const SCROLL_AMT = 200.0
 func _process(delta):
-	if Input.is_action_just_pressed("ui_down"):
-		$ScrollContainer.scroll_vertical += SCROLL_AMT
-	elif Input.is_action_just_pressed("ui_up"):
-		$ScrollContainer.scroll_vertical -= SCROLL_AMT
+	if Input.is_action_pressed("ui_down"):
+		%ScrollContainer.scroll_vertical += SCROLL_AMT * delta
+	elif Input.is_action_pressed("ui_up"):
+		%ScrollContainer.scroll_vertical -= SCROLL_AMT * delta
